0043e690f62ca60a3e17281badbf1fd22f916bab
BRI-05 <legogalore12@gmail.com>
2025-02-22 16:35:38 +0000

    v2.0.1
    Fixed error with insert methods

--- sqlMethod.py
+++ sqlMethod.py
@@ -59,21 +59,29 @@ def setup():
 
 #Creates new user
 def newUser(fn, ln, id, un, pw):
-    cursor.execute('''INSERT INTO users VALUES(?, ?, ?, ?, ?);'''), (fn, ln, id, un, pw)
+    cursor.execute('''INSERT INTO users VALUES(?, ?, ?, ?, ?)''', (fn, ln, id, un, pw))
 #Adds a new user to the group
 def appendGroup(user, group, gID):
-    cursor.execute('''INSERT INTO groupusers VALUES(?, ?, ?)'''), (user, group, gID)
+    cursor.execute('''INSERT INTO groupusers VALUES(?, ?, ?)''', (user, group, gID))
 #Creates a new group, then adds a user to the group
 def newGroup(g, gname, user, gID):
-    cursor.execute('''INSERT INTO group VALUES(?, ?)'''), (g, gname)
+    cursor.execute('''INSERT INTO group VALUES(?, ?)''', (g, gname))
     appendGroup(user, g, gID)
 #Adds a new item to a list
 def appendList(iID, lID, days ,siID):
-    cursor.execute('''INSERT INTO shoppingListItems VALUES(?, ?, ?, ?)''')
+    cursor.execute('''INSERT INTO shoppingListItems VALUES(?, ?, ?, ?)''', (iID, lID, days, siID))
 #Creates a new list, then adds an item to the list
 def newList(lID, gID, iID, days, siID):
-    cursor.execute('''INSERT INTO shoppingList VALUES(?, ?)'''), (lID, gID)
+    cursor.execute('''INSERT INTO shoppingList VALUES(?, ?)''', (lID, gID))
     appendList(iID, lID, days, siID)
 #Creates a new item
 def newItem(iID, name, price, quantity):
-    cursor.execute('''INSERT INTO item VALUES(?, ?, ?, ?)'''), (iID, name, price, quantity)
+    cursor.execute('''INSERT INTO item VALUES(?, ?, ?, ?)''', (iID, name, price, quantity))
+
+#setup()
+#newUser('Brandon', 'Ryan-Izzard', 1, 'Brizzard', 'no')
+#newItem(1, "Milk", 2.05, 500)
+#cursor.execute('''SELECT * FROM users;''')
+#print(cursor.fetchall())
+#cursor.execute('''SELECT * FROM item;''')
+#print(cursor.fetchall())
