819e138a8cb5c4ddf1a69d326d369eb3f4c32125
BRI-05 <legogalore12@gmail.com>
2025-02-22 14:00:11 +0000

    SQL Code
    Currently contains bare basics. Lays out relationships between tables, which contain minimal headings.

---
+++ shoppinglist.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2025 at 02:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `autorecieptapp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shoppinglist`
+--
+
+CREATE TABLE `shoppinglist` (
+  `UserID` int(11) DEFAULT NULL,
+  `ListID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shoppinglist`
+--
+ALTER TABLE `shoppinglist`
+  ADD PRIMARY KEY (`ListID`),
+  ADD KEY `UserID` (`UserID`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `shoppinglist`
+--
+ALTER TABLE `shoppinglist`
+  ADD CONSTRAINT `shoppinglist_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
