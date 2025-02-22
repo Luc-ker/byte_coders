0770abe0614c0e4656a3072a7d815c3e704d49c5
BRI-05 <legogalore12@gmail.com>
2025-02-22 19:01:17 +0000

    v3
    Added toDB method which reads the bill_items.csv file and converts it to a format suitable for the item table.

--- sqlMethod.py
+++ sqlMethod.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, csv
 conn = sqlite3.connect('example.db')
 cursor = conn.cursor()
 #Creates whole database
@@ -24,7 +24,7 @@ def setup():
     `ItemID` int(11) PRIMARY KEY,
     `Name` varchar(40) DEFAULT NULL,
     `Price` decimal(4,2) DEFAULT NULL,
-    `Quantity` int(11) DEFAULT NULL
+    `Store` VARCHAR(20) DEFAULT NULL
     );''')
     #ShoppingList - Links groups to lists of items
     cursor.execute('''
@@ -62,22 +62,34 @@ def newUser(fn, ln, id, un, pw):
     cursor.execute('''INSERT INTO users VALUES(?, ?, ?, ?, ?)''', (fn, ln, id, un, pw))
 #Adds a new user to the group
 def appendGroup(user, group, gID):
-    cursor.execute('''INSERT INTO groupusers VALUES(?, ?, ?)''', (user, group, gID))
+    cursor.execute('''INSERT INTO groupsusers VALUES(?, ?, ?)''', (user, group, gID))
 #Creates a new group, then adds a user to the group
 def newGroup(g, gname, user, gID):
-    cursor.execute('''INSERT INTO group VALUES(?, ?)''', (g, gname))
+    cursor.execute('''INSERT INTO groups VALUES(?, ?)''', (g, gname))
     appendGroup(user, g, gID)
 #Adds a new item to a list
 def appendList(iID, lID, days ,siID):
-    cursor.execute('''INSERT INTO shoppingListItems VALUES(?, ?, ?, ?)''', (iID, lID, days, siID))
+    cursor.execute('''INSERT INTO shoppingListItem VALUES(?, ?, ?, ?)''', (iID, lID, days, siID))
 #Creates a new list, then adds an item to the list
 def newList(lID, gID, iID, days, siID):
     cursor.execute('''INSERT INTO shoppingList VALUES(?, ?)''', (lID, gID))
     appendList(iID, lID, days, siID)
 #Creates a new item
-def newItem(iID, name, price, quantity):
-    cursor.execute('''INSERT INTO item VALUES(?, ?, ?, ?)''', (iID, name, price, quantity))
+def newItem(iID, name, price, store):
+    cursor.execute('''INSERT INTO item VALUES(?, ?, ?, ?)''', (iID, name, price, store))
+#Adds the csv contents into the Items table
+#NOTE: Currently only works for format StoreName, ID, Name, Price
+def toDB():
+    with open('bills_output\\bill_items.csv', 'r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            cursor.execute('''INSERT INTO item(Store, ItemID, Name, Price) VALUES (?, ?, ?, ?)''', row)
 
+#setup()
+#toDB()
+#cursor.execute('''SELECT * FROM item;''')
+#print(cursor.fetchall())
 #setup()
 #newUser('Brandon', 'Ryan-Izzard', 1, 'Brizzard', 'no')
 #newItem(1, "Milk", 2.05, 500)
